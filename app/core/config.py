494550f3d2ad06b7ce7e98364f79922e3083a06c
Ali Bogatyrev <marksheyder06@gmail.com>
2023-01-06 16:29:26 +0300

    set up the database

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Благотворительный фонд QRKot'
+    app_description: str = 'Фонд поддержки котиков QRKot.'
+    database_url: str
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
