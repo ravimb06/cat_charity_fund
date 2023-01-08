a547ca0f56ae8f6d6560967b5fc5ac1df7a54e76
Ali Bogatyrev <marksheyder06@gmail.com>
2023-01-08 16:53:42 +0300

    set up alembic

---
+++ app/core/base.py
@@ -0,0 +1,5 @@
+"""Импорты класса Base и всех моделей для Alembic."""
+from app.core.db import Base # noqa
+from app.models.charityproject import CharityProject # noqa
+from app.models.donation import Donation # noqa
+from app.models.user import User # noqa
--- app/models/charityproject.py
+++ app/models/charityproject.py
@@ -1,14 +1,9 @@
-from sqlalchemy import (Column, DateTime, Integer, String, Text,
-                        CheckConstraint, Boolean)
+from sqlalchemy import Column, DateTime, Integer, String, Text, Boolean
 
 from app.core.db import Base
 
 
 class CharityProject(Base):
-    __table_args__ = (
-        CheckConstraint('full_amount > 0'),
-    )
-
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
     full_amount = Column(Integer, nullable=False)
