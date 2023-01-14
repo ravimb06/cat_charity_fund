9e77d6ef6745faf35a1c7c904f3bcf1b6f16ea12
Ali Bogatyrev <marksheyder06@gmail.com>
2023-01-14 16:13:00 +0300

    added some validators

--- app/api/validators.py
+++ app/api/validators.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import HTTPException
+from pydantic import PositiveInt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.charityproject import charity_project_crud
@@ -24,9 +25,45 @@ async def check_charity_project_active(
     charity_project: CharityProject,
     session: AsyncSession,
 ) -> CharityProject:
+    """Проверяет активный ли проект."""
     if charity_project.fully_invested:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
             detail='Закрытый проект нельзя редактировать!'
         )
     return charity_project
+
+
+async def check_charity_project_update(
+    project_id: int,
+    session: AsyncSession,
+    full_amount_to_update: PositiveInt
+):
+    """Проверка того, что новая требуемая сумма больше уже внесенной."""
+    db_project = await (
+        charity_project_crud.get_charity_project_by_id(
+            project_id, session
+        )
+    )
+    if db_project.invested_amount > full_amount_to_update:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail=(
+                'Новая требуемая сумма должна быть больше уже '
+                'внесенной в проект суммы'
+            )
+        )
+
+
+async def check_name_duplicate(
+    charity_project_name: str,
+    session: AsyncSession,
+) -> None:
+    project_id = await charity_project_crud.get_project_id_by_name(
+        charity_project_name, session
+    )
+    if project_id is not None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Проект с таким именем уже существует!',
+        )
