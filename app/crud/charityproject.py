5f010ae3b096cbd7de7bd65804a0e161170d6775
Ali Bogatyrev <marksheyder06@gmail.com>
2023-01-14 14:18:59 +0300

    added function for get project by id

--- app/crud/charityproject.py
+++ app/crud/charityproject.py
@@ -1,9 +1,27 @@
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models.charityproject import CharityProject
 
 
 class CRUDCharityProject(CRUDBase):
-    pass
+    
+    async def get_charity_project_by_id(
+            self,
+            project_id: int,
+            session: AsyncSession,
+    ) -> Optional[CharityProject]:
+        """Получить проект по ID."""
+        db_project = await session.execute(
+            select(CharityProject).where(
+                CharityProject.id == project_id
+            )
+        )
+        db_project = db_project.scalars().first()
+        return db_project
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
